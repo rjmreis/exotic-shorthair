ac2c2819c8d58ba362367f011d93117260fc0ae6
Ricardo Reis <rjm.reis@gmail.com>
2019-12-03 13:30:51 +0100

    Fix test

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -9,6 +9,10 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('react-native-dark-mode', () => ({
+  useDarkMode: () => false,
+}));
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
